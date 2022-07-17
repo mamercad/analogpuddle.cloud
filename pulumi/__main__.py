a2d187bb6e052cc701356a2fc8a7178118d3b088
Mark Mercado <mamercad@gmail.com>
2022-07-17 12:48:16 -0400

    Run `psf/black`

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -7,21 +7,25 @@ domain_analogpuddle_cloud = digitalocean.Domain(
     opts=pulumi.ResourceOptions(protect=True),
 )
 
-domain_record_stackstorm_a = digitalocean.DnsRecord("stackstorm_record",
+domain_record_stackstorm_a = digitalocean.DnsRecord(
+    "stackstorm_record",
     domain="analogpuddle.cloud",
     name="stackstorm",
     ttl=300,
     type="A",
     value="134.209.170.133",
-    opts=pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
-domain_record_web_cname = digitalocean.DnsRecord("web_record",
+domain_record_web_cname = digitalocean.DnsRecord(
+    "web_record",
     domain="analogpuddle.cloud",
     name="web",
     ttl=300,
     type="CNAME",
     value="mamercad.github.io.",
-    opts=pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
 
 droplet_stackstorm = digitalocean.Droplet(
     "stackstorm",
