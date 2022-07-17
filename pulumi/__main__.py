ecb871fcffb46f186309ef21abd1d4d0341ce8e7
Mark Mercado <mamercad@gmail.com>
2022-07-17 12:27:33 -0400

    Adding domain records

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,13 +1,29 @@
 import pulumi
 import pulumi_digitalocean as digitalocean
 
-analogpuddle_cloud = digitalocean.Domain(
+domain_analogpuddle_cloud = digitalocean.Domain(
     "analogpuddle.cloud",
     name="analogpuddle.cloud",
     opts=pulumi.ResourceOptions(protect=True),
 )
 
-stackstorm = digitalocean.Droplet(
+domain_record_stackstorm_a = digitalocean.DnsRecord("stackstorm_record",
+    domain="analogpuddle.cloud",
+    name="stackstorm",
+    ttl=300,
+    type="A",
+    value="134.209.170.133",
+    opts=pulumi.ResourceOptions(protect=True))
+
+domain_record_web_cname = digitalocean.DnsRecord("web_record",
+    domain="analogpuddle.cloud",
+    name="web",
+    ttl=300,
+    type="CNAME",
+    value="mamercad.github.io.",
+    opts=pulumi.ResourceOptions(protect=True))
+
+droplet_stackstorm = digitalocean.Droplet(
     "stackstorm",
     image="ubuntu-20-04-x64",
     name="stackstorm",
