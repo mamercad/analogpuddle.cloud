78f8027abf29feb6f820344e43c7d74eddb60b73
Mark Mercado <mamercad@gmail.com>
2022-07-17 12:22:25 -0400

    Add `analogpuddle.cloud` Domain

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,11 +1,18 @@
 import pulumi
 import pulumi_digitalocean as digitalocean
 
-stackstorm = digitalocean.Droplet("stackstorm",
+analogpuddle_cloud = digitalocean.Domain(
+    "analogpuddle.cloud",
+    name="analogpuddle.cloud",
+    opts=pulumi.ResourceOptions(protect=True),
+)
+
+stackstorm = digitalocean.Droplet(
+    "stackstorm",
     image="ubuntu-20-04-x64",
     name="stackstorm",
-    private_networking=True,
     region="nyc3",
     size="g-2vcpu-8gb",
     vpc_uuid="30f86d25-414e-434f-852d-993ed8d6815e",
-    opts=pulumi.ResourceOptions(protect=True))
+    opts=pulumi.ResourceOptions(protect=True),
+)
